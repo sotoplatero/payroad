7984cb27061e69f6044641f7e9ccd16fa0041fad
Damian Soto <soto.platero@gmail.com>
2022-01-13 23:29:55 -0500

    update

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,13 +1,29 @@
-<div class="hero min-h-screen bg-base-200">
-  <div class="text-center hero-content">
-    <div class="max-w-md">
-      <h1 class="mb-5 text-7xl font-bold">
-            Telewrite
-          </h1> 
-      <p class="mb-5">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
-          </p> 
-      <button class="btn btn-primary">Get Started</button>
+<script >
+import Typewriter from 'svelte-typewriter'  
+</script>
+<div class="hero min-h-screen">
+  <div class="text-center">
+    <div class="max-w-lg space-x-8">
+      <div class="uppercase text-2xl">
+        payroad<span class="text-yellow-600">&bull;</span>shop
+      </div>
+      <h1 class="mb-5 text-6xl font-extrabold">
+        Earn bitcoins <br> by selling 
+        <div class="text-black">
+          <Typewriter loop={true} interval="70" delay="10">
+            <span class="text-pink-600">ebooks</span>
+            <span class="text-blue-600">photos</span>
+            <span class="text-green-600">recipes</span>
+            <span class="text-red-600">podcasts</span>
+            <span class="text-indigo-600 ">templates</span>
+            <span class="text-purple-600 ">courses</span>
+          </Typewriter>
+        </div>
+      </h1> 
+      <p class="mb-5 text-2xl text-gray-500 font-bold">
+        The easiest way to sell just drag and drop your digital product.
+      </p> 
+      <button class="btn btn-primary btn-outline border-4 btn-lg font-extrabold text-2xl disabled">Soon!!</button>
     </div>
   </div>
 </div>
